a43d472546c4f1834cae0125a6cde3a4c72e0685
Alexander Schwarz <a.schwarz25519008@gmail.com>
2021-09-07 13:23:39 +0200

    [Release 0.1.1][Feature] Adds additional bounce functionality for the sphere

--- Assets/RotateAroundObject.cs
+++ Assets/RotateAroundObject.cs
@@ -6,17 +6,23 @@ public class RotateAroundObject : MonoBehaviour
 {
 
     public GameObject target;
+    private int direction;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(target.transform.position, Vector3.up, 60 * Time.deltaTime);
+        transform.RotateAround(target.transform.position, Vector3.up, direction*60 * Time.deltaTime);
+    }
+
+    void OnCollisionEnter()
+    {
+        direction *= -1;
     }
 }
